eef538d2fc1e4c723b850f5e767a148c290f786e
Rajendran R <rajendranr-5483@users.noreply.github.com>
2021-07-08 17:29:48 +0530

    ASP.NET core Scheduler - Tooltip custom sample

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Schedule_core_sample.Models;
+
+namespace Schedule_core_sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.datasource = GetScheduleData();
+            return View();
+        }
+        public List<ScheduleData> GetScheduleData()
+        {
+            List<ScheduleData> scheduleData = new List<ScheduleData>();
+            scheduleData.Add(new ScheduleData
+            {
+                Id = 1,
+                Subject = "Testing",
+                StartTime = new DateTime(2018, 4, 4, 11, 0, 0),
+                EndTime = new DateTime(2018, 4, 4, 13, 0, 0)
+            });
+            return scheduleData;
+        }
+
+        public class ScheduleData
+        {
+            public int Id { get; set; }
+            public string Subject { get; set; }
+            public DateTime StartTime { get; set; }
+            public DateTime EndTime { get; set; }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Show Tooltip on hovering the subject of appointment alone in Asp.Net Core scheduler
+
+A quick start project that helps you on how to show tooltip on hovering the appointment subject alone in scheduler.
+
+## How to run this application?
+To run this application, you need to either clone this repository or download the project and then navigate to its appropriate path where it has been located in your system. Open the project in Visual Studio and run the project.
+
